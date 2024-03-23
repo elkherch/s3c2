a178589b36f3714c666410bfb74a3f418963c104
CHiekhA <cheikhm@el-technology.com>
2024-03-23 21:45:24 +0000

    hello

--- s3c2/settings.py
+++ s3c2/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-88(5t+kck9*+e=43tums9ej3y5y=zru(hxx5miz2jjo(tw0c=w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
